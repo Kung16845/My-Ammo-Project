0c15f2fdd679ff728e9f8444c0c51b88dbf420f3
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2024-03-02 14:46:59 +0700

    test client host for delay

--- My Ammo Project/Assets/Gun/Bullet.cs
+++ My Ammo Project/Assets/Gun/Bullet.cs
@@ -7,23 +7,31 @@ public class Bullet : NetworkBehaviour
     public float damage;
     public Vector2 startLocal;
     public Gun gun;
-    
+
     private void Awake()
     {
-        startLocal = new Vector2(transform.position.x, transform.position.y);
+        // startLocal = new Vector2(transform.position.x, transform.position.y);
 
     }
-    private void Update()
-    {
-        if (!IsOwner) { return; }
+    // private void Update()
+    // {
+    //     if (!IsOwner) { return; }
 
-        var currenposition = new Vector2(this.transform.position.x, this.transform.position.y);
-        if (Vector2.Distance(startLocal, currenposition) > 20)
+    //     var currenposition = new Vector2(this.transform.position.x, this.transform.position.y);
+    //     if (Vector2.Distance(startLocal, currenposition) > 20.0F)
+    //     {
+    //         // gun.DestroyBulletServerRpc(GetComponent<NetworkObject>().NetworkObjectId);
+    //     }
+    // }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!IsOwner) return;
+
+        if (other.gameObject.tag == "Wall")
         {
             gun.DestroyBulletServerRpc(GetComponent<NetworkObject>().NetworkObjectId);
         }
     }
-
 
 
 }
